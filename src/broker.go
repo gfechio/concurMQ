3cd94628e5969b0e4fa5059c2bf19af89b2fb127
Giordano Fechio <gfechio@gmail.com>
2021-01-13 16:36:22 +0100

    First version for MQTT server

--- src/broker.go
+++ src/broker.go
@@ -9,9 +9,9 @@ import (
 )
 
 func broker() {
-	h := hrotti.NewHrotti(100)
+	h := hrotti.NewHrotti(100, &hrotti.MemoryPersistence{})
 	hrotti.INFO = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-	h.AddListener("test", hrotti.NewListenerConfig("tcp://0.0.0.0:1883"))
+	h.AddListener("concurMQ test", hrotti.NewListenerConfig("tcp://0.0.0.0:1883"))
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,9 @@ module app.go
 go 1.13
 
 require (
+	github.com/alsm/hrotti v0.0.0-20170418214259-087b33bb203d
+	github.com/google/uuid v1.1.4 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.7.0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 )
